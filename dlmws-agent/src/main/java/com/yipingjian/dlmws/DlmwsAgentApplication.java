1df490b5b447d7a4df8a0a2a6188a80af72155fd
yipingjian <yipingjian@sensetime.com>
2020-03-22 00:02:27 +0800

    [init]init dlmws-agent

---
+++ dlmws-agent/src/main/java/com/yipingjian/dlmws/DlmwsAgentApplication.java
@@ -0,0 +1,13 @@
+package com.yipingjian.dlmws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlmwsAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlmwsAgentApplication.class, args);
+	}
+
+}
