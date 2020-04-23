641a8cd8a153df008518ec4e6e100935f362ae06
yipingjian <731700522@qq.com>
2020-04-23 11:06:48 +0800

    [init] init commit

---
+++ dlmws-warn/src/main/java/com/yipingjian/dlmws/DlmwsWarnApplication.java
@@ -0,0 +1,13 @@
+package com.yipingjian.dlmws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlmwsWarnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DlmwsWarnApplication.class, args);
+    }
+
+}
