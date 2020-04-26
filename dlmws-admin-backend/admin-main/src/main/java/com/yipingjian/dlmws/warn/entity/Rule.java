f1c6eb5bc69674c9e438537f8548ff05f2c8838e
yipingjian <731700522@qq.com>
2020-04-27 00:23:27 +0800

    [feat] admin开发 字段更改

--- dlmws-admin-backend/admin-main/src/main/java/com/yipingjian/dlmws/warn/entity/Rule.java
+++ dlmws-admin-backend/admin-main/src/main/java/com/yipingjian/dlmws/warn/entity/Rule.java
@@ -9,6 +9,7 @@ public class Rule {
     @TableId(type = IdType.AUTO)
     private Integer id;
     private String project;
+    private String logType;
     private String keyword;
     private String type;
     private Integer threshold;
